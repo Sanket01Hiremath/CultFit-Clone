f48e0e578d6917ae382a758a9f9bad30e9089d61
Sanket Hiremath <sankethrmth1234@gmail.com>
2022-08-26 21:11:46 +0530

    pages updated and linked

---
+++ modules/navbar.js
@@ -0,0 +1,24 @@
+let relativeNavbar=()=>{
+    return `<a href="../navbar.html"><img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_120,ar_3.87,q_auto:eco,dpr_1.3125,f_auto,fl_progressive//image/test/brand-logo/vman-and-white-cult-text.png" alt=""></a>
+    <a href="../Day1/fitness.html">FITNESS</a>
+    <a href="../Mind/mindTherapy.html">MIND</a>
+    <a href="../care/care.html">CARE</a>
+    <a href="../sports/index.html">STORE</a>
+    <div>
+        <input type="text" placeholder="Banglore">
+        <button>GET APP</button>
+        <img src="https://static.cure.fit/assets/images/user-default.png" alt="">
+        <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,q_auto:eco,dpr_1.3125,f_auto,fl_progressive//image/test/cart-dark-theme.svg" alt="">
+    </div>`
+}
+
+let stickyNavbar=()=>{
+    return `<a href="../Day2/Elitepass.html">cultpass ELITE</a>
+    <a href="../Day2/Propass.html">cultpass PRO</a>
+    <a href="../Day3/Livepass.html">cultpass LIVE</a>
+    <a href="../Day3/culttransform.html">cult Transform</a>
+    <a href="../Day3/LuxuaryGyms.html">Luxuary Gym</a>
+    <button>COMPARE PLANS</button>`
+}
+
+export {relativeNavbar as nav1,stickyNavbar as nav2} 
