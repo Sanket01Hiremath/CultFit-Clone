ec2d25bc2b128fcc6f887a583a24b727ab08d2e5
omi9jan <omikhupse09@gmail.com>
2022-08-27 00:20:46 +0530

    Added knowMore page file

---
+++ Mind/mindTherapy.js
@@ -0,0 +1,6 @@
+document.getElementById("know").addEventListener(function(){
+    window.location.href="knowMore.html";
+})
+document.getElementById("mind").addEventListener(function(){
+    window.location.href="mindTherapy.html";
+})
