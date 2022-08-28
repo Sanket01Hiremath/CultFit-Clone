a49f667b7eebcfc6e0c673ece72e5a4f8cc351d4
Sanket Hiremath <sankethrmth1234@gmail.com>
2022-08-28 14:55:05 +0530

    fitness page with transition and responsivess is added

--- Day1/scripts/sidenavmenu.js
+++ Day1/scripts/sidenavmenu.js
@@ -1,12 +1,10 @@
-let navdiv=document.getElementById("navbarmenu");
-// let navb1=document.getElementById("navb1");
-// navb1.addEventListener("click",()=>{
-//     navdiv.style.display="block";
-// })
-let navclick = ()=>{
-    navdiv.style.display="block";
+let closenav=document.getElementById("navb2");
+closenav.addEventListener("click",closeNav);
+
+function openNav() {
+    document.getElementById("navbarmenu").style.width = "300px";
+  }
+  
+function closeNav() {
+    document.getElementById("navbarmenu").style.width = "0";
 }
-let navb2=document.getElementById("navb2");
-navb2.addEventListener("click",()=>{
-    navdiv.style.display="none";
-})
--- modules/navbar.js
+++ modules/navbar.js
@@ -27,10 +27,46 @@ let stickyNavbar=()=>{
 
 let sidenavmenu=()=>{
     return `<div id="sidenavbar">
-    <img id="navb1" onclick="navclick()" width="6%" style="border-radius:50%;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbAhNCKfiq9lyMYz6c8oWY5uXYvmOrf5SGWg&usqp=CAU" alt="">
+    <img id="navb1" onclick="openNav()" width="6%" style="border-radius:50%;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbAhNCKfiq9lyMYz6c8oWY5uXYvmOrf5SGWg&usqp=CAU" alt="">
     <img width="5%" src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_35,ar_1,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/curefit-logo-white.svg" alt="">
     <input type="text" placeholder="Bangalore">
 </div>`
 }
 
-export {relativeNavbar as nav1,stickyNavbar as nav2,sidenavmenu as nav3} 
+let sidenavmenubody=()=>{
+    return `
+    <a href="../navbar.html"><p class="level3">Home</p></a>
+    <div id="fit">
+        <p class="level3">Fitness</p>
+        <li><a class="greylink20" href="../Day1/fitness.html">cultpass</a></li>
+        <li><a class="greylink20" href="../Day2/Elitepass.html">cultpass ELITE</a></li>
+        <li><a class="greylink20" href="../Day2/Propass.html">cultpass PRO</a></li>
+        <li><a class="greylink20" href="../Day3/Livepass.html">cultpass LIVE</a></li>
+        <li><a class="greylink20" href="../Day3/culttransform.html">cult Transform</a></li>
+        <li><a class="greylink20" href="../Day3/LuxuaryGyms.html">Luxuary gyms</a></li>
+    </div>
+
+    <div id="care">
+        <a><p class="level3">Care</p></a>
+        <li><a class="greylink20 href="">Lab Tests</a></li>
+    </div>
+    
+    <div id="mind">
+        <a><p class="level3">Mind</p></a>
+        <li> <a class="greylink20" href="">Therapy</a> </li>
+        <li> <a class="greylink20" href="">Mindfulness</a> </li>
+    </div>
+    <a href="../sports/index.html"><p class="level3" >Store</p></a>
+    <br>
+    <hr>
+    <br>
+    <a href=""><p class="level4">Login</p></a>
+    <a href=""><p class="level4">Cart</p></a>
+    <a href=""><p class="level4">Logout</p></a>`
+}
+
+let bottombutton=()=>{
+    return `<button id="comp">Compare Plans</button>`
+}
+
+export {relativeNavbar as nav1,stickyNavbar as nav2,sidenavmenu as nav3,sidenavmenubody as body,bottombutton as compbtn} 
